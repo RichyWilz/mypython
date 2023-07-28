59161d6a23e29d7bfdb641ffb7d338e293f23a70
Kayiira Richard <richardwilsonevarist@gmail.com>
2023-07-28 12:23:34 +0300

    tried to make fucntion to make every first letter in a string capital

---
+++ capitalize.py
@@ -0,0 +1,13 @@
+# this function takes in a string and makes every first letter in it capital
+def titlecase(str):
+   array = str.split(" ")
+   print(array)
+   newarray = []
+   i = 0
+   for i in range(len(array)):
+    newarray.append(array[i].upper() + array[i].slice(1).lower())
+   return print(newarray)
+
+
+
+titlecase("I'm a little off for today")
